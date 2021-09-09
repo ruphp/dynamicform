963f893c5a9257fd4cab8cf3c5e2e321c338be49
Александр <ruphp@mail.ru>
2021-09-09 10:36:45 +0500

    Update yii2-dynamic-form.js

--- src/assets/yii2-dynamic-form.js
+++ src/assets/yii2-dynamic-form.js
@@ -208,7 +208,7 @@
 
                     widgetsOptions = widgetsOptions.reverse();
                     for (var i = identifiers.length - 1; i >= 1; i--) {
-                        identifiers[i] = $elem.closest(widgetsOptions[i].widgetItem).index();
+                        identifiers[i] = $elem.closest(widgetsOptions[i-1].widgetItem).index();
                     }
                 }
 
